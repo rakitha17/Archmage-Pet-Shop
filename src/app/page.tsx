f2bf093650c8f58f460b19ede5b7219853d5b149
sandaru20200649 <115617513+sandaru20200649@users.noreply.github.com>
2024-06-06 19:11:25 +0530

    hero banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import { CarouselComponent } from "@/components/carousel";
 import Image from "next/image";
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import "../styles/carousel.scss"
 
+
+
+ 
 export default function Home() {
-  return;
+  return (
+    <div>
+      {/* <Button variant="primary" className="btn-one">Primary</Button> */}
+      <CarouselComponent />
+    </div>
+  );
 }
