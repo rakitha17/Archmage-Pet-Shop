b31ba72df863853a2747f9c5c7bd90b524407a2b
sandaru20200649 <115617513+sandaru20200649@users.noreply.github.com>
2024-06-14 09:54:46 +0530

    modal and final fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { CarouselComponent } from "@/components/Carousel/carousel";
 import Image from "next/image";
 import { Container, Row, Col, Button } from 'react-bootstrap';
@@ -9,24 +10,26 @@ import { MapComponent } from "@/components/Map/map";
 import { NewsLetterComponent } from "@/components/NewsLetter/newsletter";
 import { BreadCrumbComponent } from "@/components/Breadcrumb/breadcrumb";
 import ModalComponent from "@/components/Modal/modal";
+import { useState } from "react";
 
+const HomePage = () => {
+  const [showPopup, setShowPopup] = useState(true);
 
+  const handleClose = () => setShowPopup(false);
 
- 
-export default function Home() {
   return (
     <div>
-
       <CarouselComponent />
-      <BreadCrumbComponent/>
-      <Furry_friendComponent/>
-      <Save50Component/>
-      <BlogComponent/>
-      <GalleryComponent/>
-      <MapComponent/>
-      <NewsLetterComponent/>
-      <ModalComponent/>
-      
+      <BreadCrumbComponent />
+      <Furry_friendComponent />
+      <Save50Component />
+      <BlogComponent />
+      <GalleryComponent />
+      <MapComponent />
+      <NewsLetterComponent />
+      <ModalComponent show={showPopup} handleClose={handleClose} />
     </div>
   );
-}
+};
+
+export default HomePage;
